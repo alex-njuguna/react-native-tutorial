b84fc3dabb26d7f9569c96fd28e164cd4dcfed7f
alex-njuguna <njugunakinuthia013@gmail.com>
2024-08-10 04:49:58 +0300

    working with flat cards

--- class3/Awesome02/components/flatCards.tsx
+++ class3/Awesome02/components/flatCards.tsx
@@ -4,8 +4,48 @@ import React from 'react'
 export default function FlatCards() {
     return (
         <View>
-            <Text>FlatCards</Text>
+            <Text style={styles.headingText}>FlatCards</Text>
+            <View style={styles.container}>
+                <View style={[styles.card, styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+                <View style={[styles.card, styles.cardTwo]}>
+                    <Text>Red</Text>
+                </View>
+                <View style={[styles.card, styles.cardThree]}>
+                    <Text>Red</Text>
+                </View>
+            </View>
         </View>
     )
 }
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingHorizontal: 8
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    card: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "33%",
+        height: 100,
+        margin: 8,
+        borderRadius: 8,
+    },
+    cardOne: {
+        backgroundColor: "crimson"
+    },
+    cardTwo: {
+        backgroundColor: "green"
+    },
+    cardThree: {
+        backgroundColor: "blue"
+    }
+})
